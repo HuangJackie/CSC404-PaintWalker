e74305056e4f1cb0fcdc08ee11e3ae1e4455a546
Jiaqian Xu <mengde.wang@mail.utoronto.ca>
2021-09-25 11:47:34 -0400

    logic for button and wall interaction

---
+++ Assets/scripts/MoveWall.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWall : MonoBehaviour
+{
+    public bool operate = false;
+    private float end_y = -0.5f;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (operate == true)
+        {
+            Vector3 pos = transform.position - new Vector3(0, 0.006f, 0);
+            if (pos.y <= end_y)
+            {
+                operate = false;
+            } else
+            {
+                transform.position = pos;
+            }
+        }
+    }
+}
