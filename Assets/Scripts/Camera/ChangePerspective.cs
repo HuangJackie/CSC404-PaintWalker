7fdb86e8b18076616746cbecd8c8861b244758c8
Mohpreet Mankoo <mohpreetmankoo@gmail.com>
2021-11-17 12:57:56 -0500

    Change camera rotation to 90 degrees

--- Assets/Scripts/Camera/ChangePerspective.cs
+++ Assets/Scripts/Camera/ChangePerspective.cs
@@ -10,61 +10,64 @@ public class ChangePerspective : MonoBehaviour
     private ControllerUtil _controllerUtil;
 
     public float rotationSpeed;
-    private float _target_y_angle = 180f;
-    private float _rot_dest = 0f;
+    private float _target_y_angle;
+    private float _rot_dest;
     
     public bool isIntervteredControl;
     private bool _changingPersective;
 
     void Start()
     {
-        isIntervteredControl = false;
         _player = GameObject.FindWithTag("Player");
-        _changingPersective = false;
         _controllerUtil = FindObjectOfType<ControllerUtil>();
+
+        _target_y_angle = 0f;
+        _rot_dest = 0f;
+        isIntervteredControl = false;
+        _changingPersective = false;
     }
 
-    // Update is called once per frame
+    // TODO: Implement inverted controller support
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q) || _controllerUtil.GetRotationChange() > 0)
         {
             _changingPersective = true;
-            _rot_dest = 180f;
+            _rot_dest = 90;
 
-            // TODO: Uncomment when 90 degree control works
-            // _rot_dest = 90;
-            // if (_target_y_angle < 250f)
-            // {
-            //     _target_y_angle = _target_y_angle + 90f;
-            // }
-            // else
-            // {
-            //     _target_y_angle = 0;
-            // }
+            if (_target_y_angle < 250f)
+            {
+                _target_y_angle += 90f;
+            }
+            else
+            {
+                _target_y_angle = 0;
+            }
         }
         else if (Input.GetKeyDown(KeyCode.E) || _controllerUtil.GetRotationChange() < 0)
         {
             _changingPersective = true;
-            _rot_dest = -180f;
+            _rot_dest = -90;
 
-            // TODO: Uncomment when 90 degree control works
-            // _rot_dest = -90;
-            // if (_target_y_angle == 0f)
-            // {
-            //     _target_y_angle = 270f;
-            // }
-            // else
-            // {
-            //     _target_y_angle = _target_y_angle - 90f;
-            // }
+            if (_target_y_angle == 0f)
+            {
+                _target_y_angle = 270f;
+            }
+            else
+            {
+                _target_y_angle -= 90f;
+            }
         }
     }
 
-    void FixedUpdate() { 
+    void FixedUpdate()
+    { 
         if (_changingPersective)
         {
-            transform.RotateAround(transform.parent.position, Vector3.up, _rot_dest * rotationSpeed * Time.deltaTime);
+            transform.RotateAround(
+                transform.parent.position, Vector3.up, _rot_dest * rotationSpeed * Time.deltaTime
+            );
+
             if (Mathf.Abs(Mathf.Abs(transform.rotation.eulerAngles.y) - _target_y_angle) < 0.1f)
             {
                 _changingPersective = false;
@@ -82,18 +85,6 @@ public class ChangePerspective : MonoBehaviour
                         -Mathf.Abs(_target_y_angle),
                         transform.eulerAngles.z
                     );
-                }
-
-                // TODO: Remove when 90-degree control is implemented
-                if (_target_y_angle == 0)
-                {
-                    isIntervteredControl = false;
-                    _target_y_angle = 180f;
-                }
-                else
-                {
-                    isIntervteredControl = true;
-                    _target_y_angle = 0f;
                 }
             }
         }
