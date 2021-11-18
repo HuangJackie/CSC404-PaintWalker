e226753f8f0a7c18372cd2d128b05ce2e61d30c5
Jiaqian Xu <mengde.wang@mail.utoronto.ca>
2021-11-18 02:19:34 -0500

    feature change compedium

--- Assets/Scripts/Environment/PaintOrb.cs
+++ Assets/Scripts/Environment/PaintOrb.cs
@@ -13,6 +13,7 @@ public class PaintOrb : Interactable, TooltipObject
     private Light lightSettings;
     private Renderer[] meshRenderers;
     private UpdateUI _updateUI;
+    
 
     private new void Start()
     {
@@ -51,17 +52,13 @@ public class PaintOrb : Interactable, TooltipObject
         ObjectStorage.paintOrbStorage.Add(this.gameObject);
     }
 
+    
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             manager.IncreasePaint(color, paintReplenished);
             gameObject.SetActive(false);
-            MoveRedo lastCommand = manager.redoCommandHandler.LatestCommand() as MoveRedo;
-            if (lastCommand)
-            {
-                lastCommand.InjectPaintPickup(this.gameObject);
-            }
         }
     }
 
--- Assets/Scripts/UI/HUD/PaintBucketIcon.cs
+++ Assets/Scripts/UI/HUD/PaintBucketIcon.cs
@@ -8,34 +8,62 @@ public class PaintBucketIcon : MonoBehaviour
     private Image paintBucketIcon;
 
     [Header("Paint Sprites")]
-    [SerializeField] private Sprite redPaintBucket;
-    [SerializeField] private Sprite greenPaintBucket;
-    [SerializeField] private Sprite yellowPaintBucket;
-    [SerializeField] private Sprite specialPaintBucket;
+    [SerializeField] private Image redPaintBucket;
+    [SerializeField] private Image greenPaintBucket;
+    [SerializeField] private Image yellowPaintBucket;
+    [SerializeField] private Image specialPaintBucket;
+    private Vector2 newSize;
+    private Vector2 originalSize;
+    private Image cur_selection;
+    private Vector2 old_pos;
+    private Vector2 new_pos;
 
     private void Awake()
     {
+        originalSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x, greenPaintBucket.rectTransform.sizeDelta.y);
+        newSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x * 1.3f, greenPaintBucket.rectTransform.sizeDelta.y * 1.3f);
+        cur_selection = yellowPaintBucket;
         paintBucketIcon = GetComponent<Image>();
     }
 
     public void SetIcon(int paintType)
     {
+        cur_selection.rectTransform.sizeDelta = originalSize;
+        if (old_pos != Vector2.zero)
+        {
+            cur_selection.transform.position = old_pos;
+        }
         switch(paintType)
         {
             case GameConstants.GREEN_PAINT:
-                paintBucketIcon.sprite = greenPaintBucket;
+                greenPaintBucket.rectTransform.sizeDelta = newSize;
+                cur_selection = greenPaintBucket;
+                old_pos = new Vector2(greenPaintBucket.transform.position.x, greenPaintBucket.transform.position.y);
+                new_pos = new Vector2(greenPaintBucket.transform.position.x - 25, greenPaintBucket.transform.position.y + 25);
+                greenPaintBucket.transform.position = new_pos;
                 break;
             case GameConstants.RED_PAINT:
-                paintBucketIcon.sprite = redPaintBucket;
+                redPaintBucket.rectTransform.sizeDelta = newSize;
+                cur_selection = redPaintBucket;
+                old_pos = new Vector2(redPaintBucket.transform.position.x, redPaintBucket.transform.position.y);
+                new_pos = new Vector2(redPaintBucket.transform.position.x - 25, redPaintBucket.transform.position.y + 25);
+                redPaintBucket.transform.position = new_pos;
                 break;
             case GameConstants.YELLOW_PAINT:
-                paintBucketIcon.sprite = yellowPaintBucket;
+                print("here");
+                yellowPaintBucket.rectTransform.sizeDelta = newSize;
+                cur_selection = yellowPaintBucket;
+                old_pos = new Vector2(yellowPaintBucket.transform.position.x, yellowPaintBucket.transform.position.y);
+                print(old_pos);
+                new_pos = new Vector2(yellowPaintBucket.transform.position.x - 25, yellowPaintBucket.transform.position.y + 20);
+                yellowPaintBucket.transform.position = new_pos;
                 break;
             case GameConstants.BLUE_PAINT:
-                paintBucketIcon.sprite = specialPaintBucket;
-                break;
-            default:
-                paintBucketIcon.sprite = redPaintBucket;
+                specialPaintBucket.rectTransform.sizeDelta = newSize;
+                cur_selection = specialPaintBucket;
+                old_pos = new Vector2(specialPaintBucket.transform.position.x, specialPaintBucket.transform.position.y);
+                new_pos = new Vector2(specialPaintBucket.transform.position.x - 25, specialPaintBucket.transform.position.y + 25);
+                specialPaintBucket.transform.position = new_pos;
                 break;
         }
     }
