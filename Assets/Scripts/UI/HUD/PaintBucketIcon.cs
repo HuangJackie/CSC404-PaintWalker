6b5446b52dd65660d7b91c9aad727ca018f760ee
Mohpreet Mankoo <mohpreetmankoo@gmail.com>
2021-11-18 14:02:05 -0500

    Simplify PaintBucketIcon.cs and rearrange diamond icons

--- Assets/Scripts/UI/HUD/PaintBucketIcon.cs
+++ Assets/Scripts/UI/HUD/PaintBucketIcon.cs
@@ -5,65 +5,84 @@ using UnityEngine.UI;
 
 public class PaintBucketIcon : MonoBehaviour
 {
-    private Image paintBucketIcon;
-
     [Header("Paint Sprites")]
     [SerializeField] private Image redPaintBucket;
     [SerializeField] private Image greenPaintBucket;
     [SerializeField] private Image yellowPaintBucket;
     [SerializeField] private Image specialPaintBucket;
-    private Vector2 newSize;
+
+    private Image currSelection;
+
+    // For changing/restoring icon sizes
     private Vector2 originalSize;
-    private Image cur_selection;
-    private Vector2 old_pos;
-    private Vector2 new_pos;
+    private Vector2 currSelectionSize;
+    private float selectionGrowFactor = 1.4f;
+
+    // For changing/restoring icon positions
+    private Vector2 originalPos;
+    private Vector3 selectionPosOffset;
 
     private void Awake()
     {
-        originalSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x, greenPaintBucket.rectTransform.sizeDelta.y);
-        newSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x * 1.3f, greenPaintBucket.rectTransform.sizeDelta.y * 1.3f);
-        cur_selection = yellowPaintBucket;
-        paintBucketIcon = GetComponent<Image>();
+        currSelection = null;
+        originalPos = Vector2.zero;
+        selectionPosOffset = new Vector3(-18f, 15f, 0f);
+
+        originalSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x,
+                                   greenPaintBucket.rectTransform.sizeDelta.y);
+        currSelectionSize = new Vector2(greenPaintBucket.rectTransform.sizeDelta.x * selectionGrowFactor,
+                                        greenPaintBucket.rectTransform.sizeDelta.y * selectionGrowFactor);
+        
+        SetIcon(GameConstants.YELLOW_PAINT);
     }
 
+    // Change the active Icon type in the HUD UI
+    // Should correspond to Player's currently selected paint
     public void SetIcon(int paintType)
     {
-        cur_selection.rectTransform.sizeDelta = originalSize;
-        if (old_pos != Vector2.zero)
+        // Revert currently selected paint icon to original size and pos first
+        if (currSelection != null)
         {
-            cur_selection.transform.position = old_pos;
+            currSelection.rectTransform.sizeDelta = originalSize;
         }
+        if (originalPos != Vector2.zero)
+        {
+            currSelection.transform.position = originalPos;
+        }
+
+        // Then choose next icon to enlarge and display as "selected"
         switch(paintType)
         {
             case GameConstants.GREEN_PAINT:
-                greenPaintBucket.rectTransform.sizeDelta = newSize;
-                cur_selection = greenPaintBucket;
-                old_pos = new Vector2(greenPaintBucket.transform.position.x, greenPaintBucket.transform.position.y);
-                new_pos = new Vector2(greenPaintBucket.transform.position.x - 25, greenPaintBucket.transform.position.y + 25);
-                greenPaintBucket.transform.position = new_pos;
+                currSelection = greenPaintBucket;
+                greenPaintBucket.rectTransform.sizeDelta = currSelectionSize;
+                originalPos = new Vector2(greenPaintBucket.transform.position.x,
+                                          greenPaintBucket.transform.position.y);
+                greenPaintBucket.transform.position += selectionPosOffset;
                 break;
+
             case GameConstants.RED_PAINT:
-                redPaintBucket.rectTransform.sizeDelta = newSize;
-                cur_selection = redPaintBucket;
-                old_pos = new Vector2(redPaintBucket.transform.position.x, redPaintBucket.transform.position.y);
-                new_pos = new Vector2(redPaintBucket.transform.position.x - 25, redPaintBucket.transform.position.y + 25);
-                redPaintBucket.transform.position = new_pos;
+                currSelection = redPaintBucket;
+                redPaintBucket.rectTransform.sizeDelta = currSelectionSize;
+                originalPos = new Vector2(redPaintBucket.transform.position.x,
+                                          redPaintBucket.transform.position.y);
+                redPaintBucket.transform.position += selectionPosOffset;
                 break;
+
             case GameConstants.YELLOW_PAINT:
-                print("here");
-                yellowPaintBucket.rectTransform.sizeDelta = newSize;
-                cur_selection = yellowPaintBucket;
-                old_pos = new Vector2(yellowPaintBucket.transform.position.x, yellowPaintBucket.transform.position.y);
-                print(old_pos);
-                new_pos = new Vector2(yellowPaintBucket.transform.position.x - 25, yellowPaintBucket.transform.position.y + 20);
-                yellowPaintBucket.transform.position = new_pos;
+                currSelection = yellowPaintBucket;
+                yellowPaintBucket.rectTransform.sizeDelta = currSelectionSize;
+                originalPos = new Vector2(yellowPaintBucket.transform.position.x,
+                                          yellowPaintBucket.transform.position.y);
+                yellowPaintBucket.transform.position += selectionPosOffset;
                 break;
+
             case GameConstants.BLUE_PAINT:
-                specialPaintBucket.rectTransform.sizeDelta = newSize;
-                cur_selection = specialPaintBucket;
-                old_pos = new Vector2(specialPaintBucket.transform.position.x, specialPaintBucket.transform.position.y);
-                new_pos = new Vector2(specialPaintBucket.transform.position.x - 25, specialPaintBucket.transform.position.y + 25);
-                specialPaintBucket.transform.position = new_pos;
+                currSelection = specialPaintBucket;
+                specialPaintBucket.rectTransform.sizeDelta = currSelectionSize;
+                originalPos = new Vector2(specialPaintBucket.transform.position.x,
+                                          specialPaintBucket.transform.position.y);
+                specialPaintBucket.transform.position += selectionPosOffset;
                 break;
         }
     }
