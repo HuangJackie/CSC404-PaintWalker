ef49f6a99d1cf90765695775abf26c133c9c6e0f
Mohpreet Mankoo <mohpreetmankoo@gmail.com>
2021-11-26 14:20:33 -0500

    Separate MainMenu logo from BG

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DefaultNamespace;
 
 public class MainMenu : Menu
 {
+    protected GameObject menuRenderer;
+    [SerializeField] private Image gameLogo;
+
     private OptionsMenu optionsMenu;
     private LevelSelectMenu levelSelectMenu;
 
     protected override void Start()
     {
-        base.Start();
+        // First child should be the renderer for MainMenu
+        menuRenderer = transform.GetChild(0).gameObject;
+        menuRenderer.SetActive(true);
+
+        // Do same initialization as parenting class Menu
+        controllerUtil = FindObjectOfType<ControllerUtil>();
+        // Populate buttons from children of menuRenderer
+        PopulateButtons(menuRenderer.transform);
+
+        // Prepare SecondaryMenus
         optionsMenu = FindObjectOfType<OptionsMenu>();
         levelSelectMenu = FindObjectOfType<LevelSelectMenu>();
     }
