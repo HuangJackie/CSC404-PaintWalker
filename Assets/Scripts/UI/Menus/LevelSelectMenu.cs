9266f25e03c1c65bcd697f84887a5ef8249fd2ec
Hayden McGowan <hayden.patrick.mcgowan@gmail.com>
2021-12-06 01:21:56 -0500

    fixed menu music, added orb pickup sound
    - fixed menu music to not persist on scene change and to fade-out upon scene selection
    
    - added orb pickup sounds for all scenes

--- Assets/Scripts/UI/Menus/LevelSelectMenu.cs
+++ Assets/Scripts/UI/Menus/LevelSelectMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static GameConstants;
+using UnityEngine.Audio;
 
 // NOTE: This class need constant maintenance and
 // addition/removal of level-loading methods as new
@@ -9,41 +10,55 @@ using static GameConstants;
 public class LevelSelectMenu : SecondaryMenu
 {
     private Levels levelToLoad;
+    public AudioSource menuMusic;
+    public AudioMixerSnapshot menuMusicOut;
    
     public void LoadTutorialColors()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.TutorialColors;
+        menuMusicOut.TransitionTo(2f);
+
     }
 
     public void LoadAlpha()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.AlphaScenev2;
+        menuMusicOut.TransitionTo(2f);
+
     }
     
     public void LoadTut1()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.Tutorial1;
+        menuMusicOut.TransitionTo(2f);
+
     }
 
     public void LoadTut15()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.Tutorial15;
+        menuMusicOut.TransitionTo(2f);
+
     }
     
     public void LoadTut2()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.Tutorial2;
+        menuMusicOut.TransitionTo(2f);
+
     }
 
     public void LoadLevel1()
     {
         base.transitionAnimation.SetTrigger("FadeOut");
         levelToLoad = Levels.Level1;
+        menuMusicOut.TransitionTo(2f);
+
     }
 
     public void OnFadeComplete()
