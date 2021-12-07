a1b702bc9bb6dcef8ed613c1869a657348616470
Hayden McGowan <hayden.patrick.mcgowan@gmail.com>
2021-12-07 00:44:56 -0500

    music, sfx, and ambience assets added & implemented
    - fixed various audio bugs
    - added new scripts and objects related to audio
    - added sfx OutOfPaint
    - added Tutorial Music
    - added cutscene2 music

--- Assets/Scripts/Audio/MusicClipSwapTutorial2.cs
+++ Assets/Scripts/Audio/MusicClipSwapTutorial2.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class MusicClipSwapTutorial2 : MonoBehaviour
 {
+    public AudioSource music;
+    public AudioClip tutorial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        music.clip = tutorial;
+        music.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
